3f12c840a181be2b8dab9944d0d91bc2e3a31e80
= <vincent.qi@hotmail.com>
2020-12-14 22:25:01 -0800

    Updated DrawingWithText Assignment

--- DrawingWithText/src/Main.java
+++ DrawingWithText/src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
     public static void main(String[] args) {
+        private String red =
+
         // Prints out a image in the form a String
-        System.out.println("TBD");
+        System.out.println("\u0003                                    \n" +
+                "                                    /\\\n" +
+                "                                   /c\\\\\n" +
+                "                                  ‘ch.\\\\\n" +
+                "                                _/`’ <\\c;\n" +
+                "      \\%-.__                    \\   ,  \\c\\\n" +
+                "       \\\\hc`:=--._              /  _\\  .hc|\n" +
+                "        \\ `hhcccccc``~-._.^_.--`-~` <    \\c\\\n" +
+                "        <\\   `.hcccccccccccccchhhhhhh^`.  |ccc\\\n" +
+                "          / ^   `.hhccccccccccccccccccccc./hcc|\\\n" +
+                "          `~\\~__/`hhhcccccccccccccccccccccccc`\\\n" +
+                "             |h cc/ccccc cccc|hc/|hc\\hccc\\hcc\\hc|\n" +
+                "             /hhhccc/\\hhhhhcc|c/ \\hcc|cccc|ccc\\hc\\\n" +
+                "            |hhccc|/ |hhhhhh/|c!  \\hc|cccc|cccc\\#|\n" +
+                "            /hhhcc|   \\hhhhh!%/    \\h|cccc|ccccc`=-._\n" +
+                "          ./hhhhh#|    \\hhc/|/     \\/cccc/ccccchhh#/\n" +
+                "          |c/hccc%      `\\c//        \\ccccccchh`~=^`\n" +
+                "           /hhcc|| |! !|  ``   |!  ! /cccccccchhhh|\\\n" +
+                "         _/hhhhc|\\  `^^’       \\::;/ |cccc|\\hhhhhh|c%/\n" +
+                "        \\hhccccc|%\\       W          |cccc|hhhcccc\\=`\n" +
+                "         `-|cccc|h|`-..___________..~/cccc|hhccccccc|;\n" +
+                "           |cccc|h| \\hhccc\\       /h/hhccc|hhhhccc`\\\n" +
+                "           \\hccc\\hc\\ \\hhhc/       `-/ccccc/hhhhhhhh/   /\\\n" +
+                "            \\hccc\\`=` \\.”          /ccccc/|hhhhhh\\    /cc\\\n" +
+                "             \\\\;;;/    `\\_/`VVV./\\/_..-’` `\\_/`’   .*`cch|\n" +
+                "                        \\\\  \\VV/   //\\_          _-/cccchh/\n" +
+                "                        ||  |V/   // /`-_  __.-`cccccchh/\n" +
+                "                        ||  \\/   //    /  \\ccccchhh_.=`\n" +
+                "                      .-\\\\_.‘.__||  ’=.__/``-\\%%;-’`\n" +
+                "                      \\vvv/`\\vvv/_/     ,_  \\\n" +
+                "                        \\____-’    \\__-’  `-’\n");
     }
 }
