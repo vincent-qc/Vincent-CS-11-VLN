08d3a92a702f66e781cac35ce5e5a8e820664e3b
= <vincent.qi@hotmail.com>
2020-12-14 23:17:20 -0800

    Updated DrawingWithText Assignment

--- DrawingWithText/src/Main.java
+++ DrawingWithText/src/Main.java
@@ -7,36 +7,36 @@ public class Main {
         String rd = "\u001b[31m"; // Red
 
         // Prints out a image in the form a String
-        System.out.println("\u0003                                                    \n" +
-                "                                    /\\                              \n" +
-                "                                   /" + bu + "c" + r + "\\\\                            \n" +
-                "                                  ‘" + bu + "ch" + r + ".\\\\                           \n" +
-                "                                _/`’ <\\" + bu + "c" + r + ";\n" +
-                "      \\%-.__                    \\   ,  \\" + bu + "c" + r + "\\\n" +
-                "       \\\\" + bu + "hc" + r + "`:=--._              /  _\\  ." + bu + "hc" + r + "|\n" +
-                "        \\ `h" + bu + "hcccccc" + r + "``~-._.^_.--`-~` <    \\" + bu + "c" + r + "\\\n" +
-                "        <\\   `.h" + bu + "cccccccccccccchhhhhhh" + r + "^`.  |" + bu + "ccc" + r + "\\\n" +
-                "          / ^." + r + "   `h" + bu + "hccccccccccccccccccccc" + r + "./" + bu + "hcc" + r + "|\\\n" +
-                "          `~\\~__/`" + bu + "hhhcccccccccccccccccccccccc" + r + "`\\\n" +
-                "             |" + bu + "hhcc" + r + "/" + bu + "ccccc cccc" + r + "|" + bu + "hc" + r + "/|" + bu +"hc" + r + "\\" + bu + "hccc" + r + "\\" + bu + "hcc" + r + "\\" + bu + "hc" + r + "|\n" +
-                "             /" + bu + "hhhccc" + r + "/\\" + bu + "hhhhhcc" + r + "|" + bu + "c" + r + "/ \\" + bu + "hcc" + r + "|" + bu + "cccc" + r + "|" + bu + "ccc" + r + "\\" + bu + "hc" + r +"\\\n" +
-                "            |" + bu + "hhccc" + r + "|/ |" + bu + "hhhhhh"+ r + "/|" + bu + "c" + r + "|  \\" + bu + "hc" + r + "|" + bu + "cccc" + r + "|" + bu + "cccc" + r + "\\" + bu + "c" + r +"|\n" +
-                "            /" + bu + "hhhcc" + r + "|   \\" + bu + "hhhhh" + r + "!" + bu + "%" + r + "/    \\" + bu + "h" + r + "|" + bu + "cccc" + r + "|" + bu + "ccccc" + r +"`=-._\n" +
-                "          ./" + bu + "hhhhhh" + r + "|    \\" + bu + "hhc" + r + "/|/     \\/" + bu + "cccc" + r +"/" + bu + "ccccchhhh" + r +"/\n" +
-                "          |" + bu + "c" + r + "/" + bu + "hccc" + r + "%      `\\" + bu + "c" + r + "//        \\" + bu + "ccccccchh" + r + "`~=^`\n" +
-                "           /" + bu + "hhcc" + r + "|| |! !|  ``   |!  ! /" + bu + "cccccccchhhh" + r + "|\\\n" +
-                "         _/" + bu + "hhhhc" + r + "|\\  `^^’       \\::;/ |" + bu + "cccc" + r + "|\\" + bu + "hhhhhh" + r + "|" + bu + "c" + r + "%/\n" +
-                "        \\" + bu + "hhccccc" + r + "|" + bu + "h" + r + "\\       W          |" + bu + "cccc" + r + "|" + bu + "hhhcccc" + r + "\\=`\n" +
-                "         `-|" + bu + "cccc" + r + "|" + bu + "h" + r + "|`-..___________..~/" + bu + "cccc" + r + "|" + bu + "hhccccccc" + r + "|;\n" +
-                "           |" + bu + "cccc" + r + "|" + bu + "h" + r + "| \\" + bu + "hhccc" + r + "\\" + bk + "ccccccc" + r + "/" + bu + "h" + r + "/" + bu + "hhccc" + r + "|" + bu + "hhhhccc" + r + "`\\\n" +
-                "           \\" + bu + "hccc" + r + "\\" + bu + "hc" + r + "\\ \\" + bu + "hhhc" + r + "/" + bk + "ccccccc" + r + "`-/" + bu + "ccccc" + r + "/" + bu + "hhhhhhhh" + r + "/   /\\\n" +
-                "            \\" + bu + "hccc" + r + "\\`=` \\.`" + bk + "cccccccccc" + r + "/" + bu + "ccccc" + r + "/|" + bu + "hhhhhh" + r + "\\    /" + bu + "cc" + r + "\\\n" +
-                "             \\\\;;/     `\\_/`" + rd + "VVV" + r + "./\\/_..-’` `\\_/`’   .*`" + bu + "cch" + r + "|\n" +
-                "                        \\\\  \\" + rd + "VV" + r + "/   //\\_          _-/" + bu + "cccchh" + r + "/\n" +
-                "                        ||  |" + rd + "V" + r + "/   // /`-_  __.-`" + bu + "cccccchh" + r + "/\n" +
-                "                        ||  \\/   //    /  \\" + bu + "ccccchhh" + r + "_.=`\n" +
-                "                      .-\\\\_.‘.__||  ’=.__/``-\\%%;-’`\n" +
-                "                      \\vvv/`\\vvv/_/     ,_  \\\n" +
-                "                        \\____-’    \\__-’  `-’\n");
+        System.out.println("\u0003                                               " +
+                "\n" + "                                    /\\" +
+                "\n" + "                                   /" + bu + "c" + r + "\\\\" +
+                "\n" + "                                  ‘" + bu + "ch" + r + ".\\\\" +
+                "\n" + "                                _/`’ <\\" + bu + "c" + r + ";" +
+                "\n" + "      \\%-.__                    \\   ,  \\" + bu + "c" + r + "\\" +
+                "\n" + "       \\\\" + bu + "hc" + r + "`:=--._              /  _\\  ." + bu + "hc" + r + "|" +
+                "\n" + "        \\ `h" + bu + "hcccccc" + r + "``~-._.^_.--`-~` <    \\" + bu + "c" + r + "\\" +
+                "\n" + "        <\\   `.h" + bu + "cccccccccccccchhhhhhh" + r + "^`.  |" + bu + "ccc" + r + "\\" +
+                "\n" + "          / ^." + r + "   `h" + bu + "hccccccccccccccccccccc" + r + "./" + bu + "hcc" + r + "|\\" +
+                "\n" + "          `~\\~__/`" + bu + "hhhcccccccccccccccccccccccc" + r + "`\\" +
+                "\n" + "             |" + bu + "hhcc" + r + "/" + bu + "ccccc cccc" + r + "|" + bu + "hc" + r + "/|" + bu +"hc" + r + "\\" + bu + "hccc" + r + "\\" + bu + "hcc" + r + "\\" + bu + "hc" + r + "|" +
+                "\n" + "             /" + bu + "hhhccc" + r + "/\\" + bu + "hhhhhcc" + r + "|" + bu + "c" + r + "/ \\" + bu + "hcc" + r + "|" + bu + "cccc" + r + "|" + bu + "ccc" + r + "\\" + bu + "hc" + r +"\\" +
+                "\n" + "            |" + bu + "hhccc" + r + "|/ |" + bu + "hhhhhh"+ r + "/|" + bu + "c" + r + "|  \\" + bu + "hc" + r + "|" + bu + "cccc" + r + "|" + bu + "cccc" + r + "\\" + bu + "c" + r +"|" +
+                "\n" + "            /" + bu + "hhhcc" + r + "|   \\" + bu + "hhhhh" + r + "!" + bu + "%" + r + "/    \\" + bu + "h" + r + "|" + bu + "cccc" + r + "|" + bu + "ccccc" + r +"`=-._" +
+                "\n" + "          ./" + bu + "hhhhhh" + r + "|    \\" + bu + "hhc" + r + "/|/     \\/" + bu + "cccc" + r +"/" + bu + "ccccchhhh" + r +"/" +
+                "\n" + "          |" + bu + "c" + r + "/" + bu + "hccc" + r + "%      `\\" + bu + "c" + r + "//        \\" + bu + "ccccccchh" + r + "`~=^`" +
+                "\n" + "           /" + bu + "hhcc" + r + "|| |! !|  ``   |!  ! /" + bu + "cccccccchhhh" + r + "|\\" +
+                "\n" + "         _/" + bu + "hhhhc" + r + "|\\  `^^’       \\::;/ |" + bu + "cccc" + r + "|\\" + bu + "hhhhhh" + r + "|" + bu + "c" + r + "%/" +
+                "\n" + "        \\" + bu + "hhccccc" + r + "|" + bu + "h" + r + "\\       W          |" + bu + "cccc" + r + "|" + bu + "hhhcccc" + r + "\\=`" +
+                "\n" + "         `-|" + bu + "cccc" + r + "|" + bu + "h" + r + "|`-..___________..~/" + bu + "cccc" + r + "|" + bu + "hhccccccc" + r + "|;" +
+                "\n" + "           |" + bu + "cccc" + r + "|" + bu + "h" + r + "| \\" + bu + "hhccc" + r + "\\" + bk + "ccccccc" + r + "/" + bu + "h" + r + "/" + bu + "hhccc" + r + "|" + bu + "hhhhccc" + r + "`\\" +
+                "\n" + "           \\" + bu + "hccc" + r + "\\" + bu + "hc" + r + "\\ \\" + bu + "hhhc" + r + "/" + bk + "ccccccc" + r + "`-/" + bu + "ccccc" + r + "/" + bu + "hhhhhhhh" + r + "/   /\\" +
+                "\n" + "            \\" + bu + "hccc" + r + "\\`=` \\.`" + bk + "cccccccccc" + r + "/" + bu + "ccccc" + r + "/|" + bu + "hhhhhh" + r + "\\    /" + bu + "cc" + r + "\\" +
+                "\n" + "             \\\\;;/     `\\_/`" + rd + "VVV" + r + "./\\/_..-’` `\\_/`’   .*`" + bu + "cch" + r + "|" +
+                "\n" + "                        \\\\  \\" + rd + "VV" + r + "/   //\\_          _-/" + bu + "cccchh" + r + "/" +
+                "\n" + "                        ||  |" + rd + "V" + r + "/   // /`-_  __.-`" + bu + "cccccchh" + r + "/" +
+                "\n" + "                        ||  \\/   //    /  \\" + bu + "ccccchhh" + r + "_.=`" +
+                "\n" + "                      .-\\\\_.‘.__||  ’=.__/``-\\%%;-’`" +
+                "\n" + "                      \\vvv/`\\vvv/_/     ,_  \\" +
+                "\n" + "                        \\____-’    \\__-’  `-’");
     }
 }
