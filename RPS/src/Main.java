710b29c083e49014fc0c71d1f0812b67c3c79e87
= <vincent.qi@hotmail.com>
2020-12-17 16:15:48 -0800

    Added Documentation

--- RPS/src/Main.java
+++ RPS/src/Main.java
@@ -6,21 +6,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Initialize the variables
         HashMap<String, String> winningValues = initializeValues();
         List<String> possibleValues = Arrays.asList("rock", "paper", "scissors", "r", "p", "s");
         Scanner scan = new Scanner(System.in);
-        boolean playing = true;
 
+        // Repeats the game if the user does not Quit
         while (true) {
+
+            // Get User + Bot choices
             String userChoice = scan.nextLine().toLowerCase();
             String botChoice = possibleValues.get((int) (Math.random() * 3 + 3));
 
-            System.out.println(botChoice);
-            if (!possibleValues.contains(userChoice)) {
+            // Checks if User Input is Q or QUIT
+            if(userChoice.equals("q") || userChoice.equals("quit")) {
+                break;
+            } else if (!possibleValues.contains(userChoice)) {
+
+                // Checks if User Input is valid
                 System.out.println("Sorry, " + userChoice + " was not a Valid Input :/");
                 continue;
             }
 
+            // Compares bot value to user value and determines the winner
             if (botChoice.equals(userChoice)) {
                 System.out.println("Draw");
             } else if (botChoice.equals(winningValues.get(userChoice))) {
@@ -32,7 +41,14 @@ public class Main {
         }
     }
 
+    /**
+     * Creates and Returns HashMap => Key: String, Value: String of the winning RPS values
+     *
+     * @return Returns a HashMap with the Winning Values for RPS
+     */
     private static HashMap<String, String> initializeValues() {
+
+        // Creates the HashMap
         HashMap<String, String> values = new HashMap<>();
         values.put("r", "s");
         values.put("s", "p");
@@ -41,8 +57,16 @@ public class Main {
         return values;
     }
 
-    private static String getFullName(String word) {
-        switch (word) {
+    /**
+     * Takes in a Letter and Returns the full name
+     *
+     * @param letter The input letter
+     * @return Returns a String with the full name of the letter
+     */
+    private static String getFullName(String letter) {
+
+        // Switches between the letters
+        switch (letter) {
             case "r":
                 return "Rock";
             case "p":
@@ -52,5 +76,4 @@ public class Main {
         }
         return null;
     }
-
 }
