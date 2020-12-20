24dd7d8c1a2da09051548300db36130909a77e72
= <vincent.qi@hotmail.com>
2020-12-19 19:15:50 -0800

    Updated String Encryption Assignment

--- String Encryption Assignment/src/Main.java
+++ String Encryption Assignment/src/Main.java
@@ -2,4 +2,34 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static String encrypt(String text) {
+        String newString = "";
+    }
+
+    private static String decrypt(String text) {
+
+    }
 }
+
+//    The example recreated
+//
+//    /**
+//     * Moves the last letter to the front via concatenation and appends "ay"
+//     *
+//     * @param text The Text to encrypt
+//     * @return Returns the encrypted text
+//     */
+//    private static String encryptString(String text) {
+//        return text.charAt(text.length() - 1) + text.substring(0, text.length() - 1) + "ay";
+//    }
+//
+//    /**
+//     * Disregards the "ay" and switches the positions of the first letter and the rest of the String
+//     *
+//     * @param text The text to decrypt
+//     * @return Returns the decrypted text
+//     */
+//    private static String decryptString(String text) {
+//        return text.substring(1, text.length() - 2) + text.charAt(0);
+//    }
