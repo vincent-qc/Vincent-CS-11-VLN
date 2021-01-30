8ddf49f5fe72ed6458fcb2a9b244b318a6c93afa
= <vincent.qi@hotmail.com>
2021-01-30 11:05:43 -0800

    Added Project 1

--- String Encryption Assignment/src/Main.java
+++ String Encryption Assignment/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        
         // Test Cases
         System.out.println(encryptString("Hello"));
         System.out.println(decryptString("oHellay"));
