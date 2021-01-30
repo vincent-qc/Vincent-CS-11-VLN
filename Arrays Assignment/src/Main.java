bc5cf67617e78efb28fa637b89263fc83ff1f463
= <vincent.qi@hotmail.com>
2021-01-29 17:59:26 -0800

    Updated Arrays Assignment

--- Arrays Assignment/src/Main.java
+++ Arrays Assignment/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        String[] stringArray = {"Foo", "Bar", "Baz"};
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 6.6};
 
+        // Test Cases
+        pushEnd(stringArray, "Hello World");
+        popEnd(intArray);
+        insert(doubleArray, 5.5, 4);
     }
 
     private static <T> void pushEnd(T[] array, T item) {
