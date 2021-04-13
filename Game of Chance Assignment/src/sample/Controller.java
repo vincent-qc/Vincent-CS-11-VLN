7c46ec3bf1b2e90784ac3d5e943fb0e7de381c52
= <vincent.qi@hotmail.com>
2021-04-12 20:45:15 -0700

    Added Documentation For Game of Chance Assignment

--- Game of Chance Assignment/src/sample/Controller.java
+++ Game of Chance Assignment/src/sample/Controller.java
@@ -1,20 +1,21 @@
 package sample;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Controller {
+    /**
+     * Note This Game Can be Easily Cheated
+     * I Didn't Give the Mechanics a Second Thought
+     * TL;DR - The Game Sucks - Focused on GUI
+     */
+
+    // Public Fields
     public TextField textGetName;
     public Button btnSubmitName;
     public Label lbBalance;
@@ -31,6 +32,7 @@ public class Controller {
     public Button btnBetYes;
     public Button btnBetNo;
 
+    // Private Fields
     private String userName;
     private int money = 100;
     private int threshold;
@@ -39,7 +41,13 @@ public class Controller {
     private Random r = new Random();
     private int[] results = new int[3];
 
+    /**
+     * Sets the player's name
+     *
+     * @param actionEvent Ignore
+     */
     public void submitName(ActionEvent actionEvent) {
+
         userName = textGetName.getText();
         lbTitle.setText("Welcome, " + userName);
 
@@ -50,22 +58,38 @@ public class Controller {
             // Bet
             vboxBet.setVisible(true);
 
-            updateUI();
+            // Sets Default Balance ($100) and Generates the Random Threshold
+            lbBalance.setText("Balance: $" + money);
             threshold = r.nextInt(4) + 6; // Random number between 6 and 9
             lbBetQuestion.setText("Will the largest number be bigger than " + threshold + "?");
         }
     }
 
+    /**
+     * The User Bet that the Largest Number will be Bigger than the Threshold
+     *
+     * @param actionEvent Ignore
+     */
     public void betYes(ActionEvent actionEvent) {
         bigger = true;
         bet();
     }
 
+    /**
+     * The User Bet that the Largest Number will be Smaller than the Threshold
+     *
+     * @param actionEvent Ignore
+     */
     public void betNo(ActionEvent actionEvent) {
         bigger = false;
         bet();
     }
 
+    /**
+     * Create a New Bet (New Threshold)
+     * Removes the Previous Round's Text
+     * Enables The Bet Buttons
+     */
     public void newBet() {
         threshold = r.nextInt(4) + 6; // Random number between 6 and 9
         lbBetQuestion.setText("Will the largest number be bigger than " + threshold + "?");
@@ -83,12 +107,21 @@ public class Controller {
 
     // Private Functions
 
+    /**
+     * Bet Using the Inputted Amount (Must be Integer)
+     * Adds / Subtracts the Amount
+     * Displays a Win / Lose Message
+     */
     private void bet() {
-        won = false;
 
+        // Set / Reset Variables
+        won = false;
         String amt = textBetAmount.getText();
         int parsedAmt;
 
+        // Try to Parse the Input to Integer
+        // Subtracts Amount from Balance
+        // Catches Exception then Logs Invalid Number (Does Not Show On GUI)
         try {
             parsedAmt = Integer.parseInt(amt);
             if((money - parsedAmt < 0) || (parsedAmt <= 0)) return;
@@ -98,25 +131,33 @@ public class Controller {
             return;
         }
 
+        // Loops Three Time to Generate Three Numbers (For Dices)
         for(int i = 0; i < 3; i++) {
             results[i] = r.nextInt(11); // Number from 1-10
+            // Checks if a Single Roll is Bigger than Threshold (If Bet Yes)
             if(bigger && results[i] > threshold) won = true;
+
+            // Checks that ALL Rolls are Smaller than the Threshold
             if(i == 2 && !bigger)
-                if(results[0] <= threshold && results[1] <= threshold && results[2] <= threshold) // Works but not efficient - Might change later
+                if(results[0] <= threshold && results[1] <= threshold && results[2] <= threshold)
                     won = true;
         }
 
+        // Disable Buttons
         btnBetNo.setDisable(true);
         btnBetYes.setDisable(true);
 
+        // Set the Roll Labels to their Values
         lbResult1.setText(String.valueOf(results[0]));
         lbResult2.setText(String.valueOf(results[1]));
         lbResult3.setText(String.valueOf(results[2]));
 
+        // Make OK Button Visible
         btnResultsOK.setVisible(true);
 
-        System.out.println(threshold + " " + won);
-
+        // Display Results
+        // If Won: Doubles Bet
+        // If Lost: Does Not Add nor Subtract (Lose Original Bet Money)
         if(won) {
             lbWinResult.setText("You won");
             money += Integer.parseInt(amt) * 2;
@@ -124,10 +165,6 @@ public class Controller {
             lbWinResult.setText("You lost");
         }
 
-        updateUI();
-    }
-
-    private void updateUI() {
         lbBalance.setText("Balance: $" + money);
     }
 }
