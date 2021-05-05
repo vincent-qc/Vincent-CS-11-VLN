237182f4f9e7b0af99e676b70f457298026df6a9
= <vincent.qi@hotmail.com>
2021-05-04 17:45:34 -0700

    Completed Reading From Text Assignment

--- Reading From Text/src/Main.java
+++ Reading From Text/src/Main.java
@@ -1,2 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // Init
+        ArrayList<String> lines = new ArrayList<>();
+        FileReader fReader = new FileReader("ProgrammingHistory.txt");
+        BufferedReader bReader = new BufferedReader(fReader);
+
+        // Read the file
+        String line;
+        while((line = bReader.readLine()) != null) lines.add(line);
+
+        // The Test
+        System.out.println(findWord("the", lines));
+    }
+
+    /**
+     * Finds a word in an arraylist of lines
+     * @param word The word to find
+     * @param lines The arraylist of lines
+     * @return Formatted String
+     */
+    private static String findWord(String word, ArrayList<String> lines) {
+        String result = "The word " + word + " can be found in these lines (starting from index 0): ";
+
+        // Loop through the lines and add them to the result
+        for(int i = 0; i < lines.size(); i++)
+            if(lines.get(i).contains(word))
+                result += (i + ", ");
+
+        return result.substring(0, result.length() - 2); // Remove the last ', '
+    }
 }
